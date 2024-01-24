58799c1779458639ab713d3cefd9fdaa338e8d56
Shiv Desai <shiv.desai99@gmail.com>
2024-01-23 23:39:36 -0500

    Commit all

---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "84e3bac5fcmshfaba2f47005e1fap155d66jsn76afcc519d8b",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "cfc4365e5a15759ff577c7b91a2b973f";
